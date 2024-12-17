d90a530d35a88596237b9cd7320456773d8b1398
Tomek <tomaszarczewski2@gmail.com>
2024-12-17 20:36:36 +0100

    Add dependencies

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from typing import Any, Dict
 from numbers import Number
 
-from hyperband import ModelTrainer
+from utils import ModelTrainer
 from prepare_data import prepare_and_load_data
 from models import GRU
 
@@ -18,31 +18,56 @@ from models import GRU
 device = torch.device('cuda')
 
 
-class Trainer(ModelTrainer):
-    def __init__(self, modelClass: Any, params: Dict[str, Any], train_dataloader: DataLoader, valid_dataloader: DataLoader, log_every: Number,
-                filename='model.pth', log_wandb=False):
+class EphemeralModelTrainer(ModelTrainer):
+    def __init__(self, Class: Any, params: Dict[str, Any], train_dataloader: DataLoader, valid_dataloader: DataLoader,
+                 log_every: int, filename='checkpoints/model.pth', log_wandb=False, epoch_save=False):
+
+        self.Class = Class
+        self.lr = params['lr']
+        self.network_params = params.copy()
+        del self.network_params['lr']
+
         self.train_dataloader = train_dataloader
         self.valid_dataloader = valid_dataloader
         self.log_every = log_every
-        self.params = params
         self.filename = filename
-        self.modelClass = modelClass
         self.log_wandb = log_wandb
-        self.lr = self.params['lr']
-        self.network_params = self.params.copy()
-        del self.network_params['lr']
-        self.model = None
-        self.optimizer = None
+        self.epoch_save = epoch_save
 
         self.criterion = nn.CrossEntropyLoss()
         self.current_training_length = 0
         self.iteration = 0
 
+        self.model = None
+        self.optimizer = None
+
         if self.log_wandb:
             wandb.init(
                 project="nationality-prediction",
                 name='gru-model',
-                config=self.params)
+                config=params)
+
+    def load(self):
+        self.model = self.Class(**self.network_params).to(device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        if os.path.exists(self.filename):
+            checkpoint = torch.load(self.filename)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            return True
+        return False
+
+    def save(self, clear_memory=True):
+        if os.path.dirname(self.filename) != '':
+            os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+        torch.save({
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+        }, self.filename)
+
+        if clear_memory:
+            self.model = None
+            self.optimizer = None
 
     def test(self, dataloader: DataLoader):
         losses = []
@@ -64,12 +89,7 @@ class Trainer(ModelTrainer):
 
     def train(self, training_length: float):
 
-        self.model = self.modelClass(**self.network_params).to(device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
-        if os.path.exists(self.filename):
-            checkpoint = torch.load(self.filename)
-            self.model.load_state_dict(checkpoint['model_state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.load()
 
         it = iter(self.train_dataloader)
 
@@ -78,6 +98,8 @@ class Trainer(ModelTrainer):
             try:
                 sequences, lengths, labels = next(it)
             except StopIteration:
+                if self.epoch_save:
+                    self.save(clear_memory=False)
                 it = iter(self.train_dataloader)
                 sequences, lengths, labels = next(it)
 
@@ -110,14 +132,7 @@ class Trainer(ModelTrainer):
         self.model.eval()
         test_loss, info = self.test(self.valid_dataloader)
 
-        if os.path.dirname(self.filename) != '':
-            os.makedirs(os.path.dirname(self.filename), exist_ok=True)
-        torch.save({
-            'model_state_dict': self.model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-        }, self.filename)
-        del self.optimizer
-        del self.model
+        self.save()
 
         if self.log_wandb:
             wandb.log({"test_loss": test_loss, "accuracy": info['accuracy']}, step=self.iteration)
@@ -140,11 +155,5 @@ if __name__ == "__main__":
     }
     num_epochs = 15
 
-    from natpred import NationalityPrediction
-    nat_pred = NationalityPrediction()
-
-    #trainer = Trainer(GRU, params, train_dataloader, valid_dataloader, 3000, log_wandb=True)
-    #trainer.train(num_epochs)
-
-    #with open('model/data.pkl', 'wb') as f:
-    #    pickle.dump((letter_encoder, nat_labels), f)
+    trainer = EphemeralModelTrainer(GRU, params, train_dataloader, valid_dataloader, 3000, log_wandb=True)
+    trainer.train(num_epochs)
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Tuple
+from numbers import Number
 import unicodedata
 
 
@@ -5,6 +8,38 @@ def remove_diacritics(text):
     normalized_text = unicodedata.normalize('NFD', text)
     stripped_text = ''.join(char for char in normalized_text if unicodedata.category(char) != 'Mn')
     return stripped_text
+
+
+class Model(ABC):
+    @abstractmethod
+    def save(self, path: str) -> None:
+        pass
+    @abstractmethod
+    def load(self, path: str) -> bool:
+        pass
+    @abstractmethod
+    def train(self, training_length: float) -> Tuple[float, Dict[str, float]]:
+        pass
+
+class ModelGenerator(ABC):
+    @abstractmethod
+    def get_model(self, params: Dict[str, Any]) -> Model:
+        pass
+
+class ModelTrainer(ABC):
+    @abstractmethod
+    def train(self, training_length: float) -> Tuple[float, Dict[str, float]]:
+        pass
+
+class TrainerGenerator(ABC):
+    @abstractmethod
+    def get_trainer(self, params: Dict[str, Any], filename: str) -> ModelTrainer:
+        pass
+
+class ParameterSampler(ABC):
+    @abstractmethod
+    def sample_parameters(self) -> Dict[str, Any]:
+        pass
 
 
 class LetterEncoder:
